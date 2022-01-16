2dca03770a847cc30c0bed9296c7b4c0225a23c9
Kogby <kogby0507@gmail.com>
2022-01-16 20:16:54 +0800

    for deploy

--- src/instance.js
+++ src/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = 'http://localhost:5000'
+const API_ROOT = 'https://fierce-reaches-50430.herokuapp.com/'
 
 const instance = axios.create({
   baseURL: API_ROOT,
